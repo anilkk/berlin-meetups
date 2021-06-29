eeb48bbdced3d229d61d15956eff6383b0b66091
Anil Kumar <anil.kumar@contentful.com>
2021-06-29 17:46:03 +0200

    Update type-form trigger

--- functions/type-form-trigger/type-form-trigger.js
+++ functions/type-form-trigger/type-form-trigger.js
@@ -1,5 +1,55 @@
 const querystring = require("querystring");
 
+const createClient = require('contentful-management').createClient;
+const { CF_MANAGEMENT_ACCESS_TOKEN, CONTENTFUL_SPACE_ID} = process.env;
+var client = createClient({
+    accessToken: CF_MANAGEMENT_ACCESS_TOKEN
+  });
+
+  let newEntryId = '';
+  let currentEnvironment = null;
+  const AddEntry = (name, url) => {
+    client.getSpace(CONTENTFUL_SPACE_ID).then((space) => {
+        space.getEnvironment('master').then((environment) => {
+            currentEnvironment = environment;
+            environment.createEntry('meetup', {
+                fields: {
+                    name: {
+                        'en-US': name
+                    },
+                    url: {
+                        'en-US': url
+                    },
+                }
+             }).then((entry) => {
+                 const {
+                     sys : {
+                        id: newlyAddEntryId
+                     }
+                 } = entry;
+                console.log('new meetup entry was successfully created', newlyAddEntryId)
+                newEntryId = newlyAddEntryId;
+                return entry;
+            })
+            .then((entry) => entry.publish())
+            .then(() => currentEnvironment.getEntry('ZJFQdPevS9JVoV7GEO9TW')) // Entry id for Meetups
+            .then((entry) => {
+                const entries = entry.fields.listOfMeetups['en-US'];
+                entries.push({"sys":{"type":"Link","linkType":"Entry","id":newEntryId}});
+                console.log('entry fields', entry.fields)
+                entry.fields.listOfMeetups['en-US'] = entries
+                return entry.update()
+            })
+            .catch(error => {
+                console.log('ERROR -->', error)
+            })
+        
+        })
+      });    
+  }
+
+
+
 exports.handler = async (event, context) => {
   // Only allow POST
   if (event.httpMethod !== "POST") {
@@ -12,7 +62,8 @@ exports.handler = async (event, context) => {
   console.log("EVENT --->", event);
   console.log("EVENT body --->", event.body);
   const name = params.name || "World";
-
+  
+  AddEntry('Product Crunch', 'https://meetup.com/product-crunch');
   return {
     statusCode: 200,
     body: `Hello, ${name}`,
