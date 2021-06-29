f808691bbe3202587d0705dedf5e7ebba28a609f
Anil Kumar <anil.kumar@contentful.com>
2021-06-29 18:38:51 +0200

    update typeform

--- functions/type-form-trigger/type-form-trigger.js
+++ functions/type-form-trigger/type-form-trigger.js
@@ -3,48 +3,6 @@ const querystring = require("querystring");
 const createClient = require('contentful-management').createClient;
 const { CF_MANAGEMENT_ACCESS_TOKEN, CONTENTFUL_SPACE_ID} = process.env;
 
-  const addEntry = async (name, url) => {
-    client.getSpace(CONTENTFUL_SPACE_ID).then((space) => {
-        space.getEnvironment('master').then((environment) => {
-            currentEnvironment = environment;
-            environment.createEntry('meetup', {
-                fields: {
-                    name: {
-                        'en-US': name
-                    },
-                    url: {
-                        'en-US': url
-                    },
-                }
-             }).then((entry) => {
-                 const {
-                     sys : {
-                        id: newlyAddEntryId
-                     }
-                 } = entry;
-                console.log('new meetup entry was successfully created', newlyAddEntryId)
-                newEntryId = newlyAddEntryId;
-                return entry;
-            })
-            .then((entry) => entry.publish())
-            .then(() => currentEnvironment.getEntry('ZJFQdPevS9JVoV7GEO9TW')) // Entry id for Meetups
-            .then((entry) => {
-                const entries = entry.fields.listOfMeetups['en-US'];
-                entries.push({"sys":{"type":"Link","linkType":"Entry","id":newEntryId}});
-                console.log('entry fields', entry.fields)
-                entry.fields.listOfMeetups['en-US'] = entries
-                return entry.update()
-            })
-            .catch(error => {
-                console.log('ERROR -->', error)
-            })
-        
-        })
-      });    
-  }
-
-
-
 exports.handler = async (event, context) => {
   var client = createClient({
     accessToken: CF_MANAGEMENT_ACCESS_TOKEN
@@ -64,10 +22,11 @@ exports.handler = async (event, context) => {
   console.log("EVENT --->", event);
   const [meetupName, meetupUrl] = event.body.form_response.answers;
   console.log("EVENT body --->", event.body.form_response.answers[]);
-  const name = params.name || "World";
+  // const name = params.name || "World";
+  
   console.log("BEFORE CREATE EVENT --->");
   console.log(meetupName.text, meetupUrl.url);
-  
+
   await client.getSpace(CONTENTFUL_SPACE_ID).then((space) => {
     space.getEnvironment('master').then((environment) => {
         currentEnvironment = environment;
@@ -109,6 +68,6 @@ exports.handler = async (event, context) => {
   
   return {
     statusCode: 200,
-    body: `Hello, ${name}`,
+    body: `Hello, world`,
   };
 };
